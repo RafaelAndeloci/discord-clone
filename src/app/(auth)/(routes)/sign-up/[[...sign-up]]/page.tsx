1795ed129a550e8f240723a78a5465317f255eba
rafael <rafaelandeloci67@gmail.com>
2024-04-22 22:05:39 -0300

    adjusting themes and auto redirecting to sign-in/sign-out when not logged in

---
+++ src/app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,7 @@
+import { SignUp } from "@clerk/nextjs";
+
+function Page() {
+  return <SignUp routing="hash" />;
+}
+
+export default Page;
---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import {
+  auth,
+  clerkMiddleware,
+  createRouteMatcher,
+} from "@clerk/nextjs/server";
+
+const isAuthenticationRoute = createRouteMatcher(["/sign-in", "/sign-up"]);
+
+export default clerkMiddleware((auth, req) => {
+  if (!isAuthenticationRoute(req)) auth().protect();
+});
+
+export const config = {
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
