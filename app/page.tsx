5d46a3b124e06b9b712fff2f9b6fd58f29f883e2
Rafael Andeloci <rafaelandeloci.dev@gmail.com>
2024-07-16 13:41:05 -0300

    feat(): initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="">
+      <p>Discord Clone</p>
+
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
